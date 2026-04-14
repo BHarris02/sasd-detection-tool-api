01f75b9a879905b875f5b3e7a0fc0ba36a4c674d
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 12:27:15 +0100

    update `NlpGatewayImpl` constructor

--- data/src/main/kotlin/com/sasd/data/gateway/NlpGatewayImpl.kt
+++ data/src/main/kotlin/com/sasd/data/gateway/NlpGatewayImpl.kt
@@ -1,5 +1,6 @@
 package com.sasd.data.gateway
 
+import com.sasd.data.client.analysis.NlpApiService
 import com.sasd.domain.entity.analysis.NlpAnalysis
 import com.sasd.domain.entity.vcs.CodeSnippet
 import com.sasd.domain.entity.vcs.Commit
@@ -7,7 +8,9 @@ import com.sasd.domain.entity.vcs.FileContent
 import com.sasd.domain.entity.vcs.Issue
 import com.sasd.domain.gateway.NlpGateway
 
-class NlpGatewayImpl(): NlpGateway {
+class NlpGatewayImpl(
+    private val nlpApiService: NlpApiService
+): NlpGateway {
     override fun analyzeCommits(commits: List<Commit>): List<NlpAnalysis> {
         TODO("Not yet implemented")
     }
