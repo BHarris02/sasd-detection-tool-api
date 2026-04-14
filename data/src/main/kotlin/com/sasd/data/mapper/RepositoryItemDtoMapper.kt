1b06052b01562953befb4bd88ec91fa97cb83ba2
BHarris02 <dev.blake.harris@gmail.com>
2026-04-14 15:57:19 +0100

    update mapper to map child items

--- data/src/main/kotlin/com/sasd/data/mapper/RepositoryItemDtoMapper.kt
+++ data/src/main/kotlin/com/sasd/data/mapper/RepositoryItemDtoMapper.kt
@@ -5,9 +5,12 @@ import com.sasd.domain.common.DomainError
 import com.sasd.domain.entity.vcs.RepositoryItem
 import com.sasd.domain.entity.vcs.RepositoryItemType
 
-fun RepositoryItemDto.toDomain() = RepositoryItem(
-    name = name,
-    path = path,
-    type = RepositoryItemType.entries.firstOrNull { it.value == type } ?: throw DomainError(),
-    children = null
-)
+fun RepositoryItemDto.toDomain(): RepositoryItem {
+    val mappedChildren: List<RepositoryItem> = children?.map { it.toDomain() } ?: emptyList()
+    return RepositoryItem(
+        name = name,
+        path = path,
+        type = RepositoryItemType.entries.firstOrNull { it.value == type } ?: throw DomainError(),
+        children = mappedChildren
+    )
+}
