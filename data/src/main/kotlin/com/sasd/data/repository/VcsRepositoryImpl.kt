296ccafd3d4c6db19f2046efab991a7ebd7bdfae
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 15:17:33 +0100

    implement interface methods (WIP)

--- data/src/main/kotlin/com/sasd/data/repository/VcsRepositoryImpl.kt
+++ data/src/main/kotlin/com/sasd/data/repository/VcsRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.sasd.data.repository
 
+import com.sasd.data.client.mapper.toDomain
 import com.sasd.data.client.vcs.VcsApiService
+import com.sasd.domain.common.DomainError
 import com.sasd.domain.entity.vcs.Commit
 import com.sasd.domain.entity.vcs.FileContent
 import com.sasd.domain.entity.vcs.Issue
@@ -10,22 +12,47 @@ import com.sasd.domain.repository.VcsRepository
 class VcsRepositoryImpl(
     private val vcsApiService: VcsApiService
 ): VcsRepository {
-    override fun getCommits(repoUrl: String): List<Commit> {
-        TODO("Not yet implemented")
+    override suspend fun getCommits(repoUrl: String): List<Commit> {
+        try {
+            val commitDtos = vcsApiService.fetchCommits(repoUrl)
+            return commitDtos.map { dto ->
+                dto.toDomain()
+            }
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun getIssues(repoUrl: String): List<Issue> {
-        TODO("Not yet implemented")
+    override suspend fun getIssues(repoUrl: String): List<Issue> {
+        try {
+            val issueDtos = vcsApiService.fetchIssues(repoUrl)
+            return issueDtos.map { dto ->
+                dto.toDomain()
+            }
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun getFileContent(
-        repoUrl: String,
-        filePath: String
-    ): FileContent {
-        TODO("Not yet implemented")
+    override suspend fun getFileContent(repoUrl: String, filePath: String): FileContent {
+        try {
+            val contentDto = vcsApiService.fetchFileContent(repoUrl, filePath)
+            return contentDto.toDomain()
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun getRepositoryItem(repoUrl: String): List<RepositoryItem> {
-        TODO("Not yet implemented")
+    override suspend fun getRepositoryItem(repoUrl: String): List<RepositoryItem> {
+        try {
+            val itemDtos = vcsApiService.fetchRepositoryStructure(repoUrl)
+            return emptyList()
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 }
