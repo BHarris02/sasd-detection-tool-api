856f21fc4fc72ff8fe1b56ebd3289dbda24c5fb8
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 12:27:25 +0100

    add build configuration for Kotlin serialization and data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+group = "com.sasd"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
