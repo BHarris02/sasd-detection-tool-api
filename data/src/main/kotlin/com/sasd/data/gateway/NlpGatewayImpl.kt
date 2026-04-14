517c2c4a8b9020a4b23f18a57cceb997111257a6
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 16:56:04 +0100

    implement interface methods

--- data/src/main/kotlin/com/sasd/data/gateway/NlpGatewayImpl.kt
+++ data/src/main/kotlin/com/sasd/data/gateway/NlpGatewayImpl.kt
@@ -1,6 +1,8 @@
 package com.sasd.data.gateway
 
 import com.sasd.data.client.analysis.NlpApiService
+import com.sasd.data.mapper.analysis.toDomain
+import com.sasd.domain.common.DomainError
 import com.sasd.domain.entity.analysis.NlpAnalysis
 import com.sasd.domain.entity.vcs.CodeSnippet
 import com.sasd.domain.entity.vcs.Commit
@@ -11,19 +13,51 @@ import com.sasd.domain.gateway.NlpGateway
 class NlpGatewayImpl(
     private val nlpApiService: NlpApiService
 ): NlpGateway {
-    override fun analyzeCommits(commits: List<Commit>): List<NlpAnalysis> {
-        TODO("Not yet implemented")
+    override suspend fun analyzeCommits(commits: List<Commit>): List<NlpAnalysis> {
+        try {
+            val analysesDtos = commits.map { commit ->
+                nlpApiService.analyzeCommit(commit.message)
+            }
+            return analysesDtos.map { dto ->
+                dto.toDomain()
+            }
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun analyzeIssues(issues: List<Issue>): List<NlpAnalysis> {
-        TODO("Not yet implemented")
+    override suspend fun analyzeIssues(issues: List<Issue>): List<NlpAnalysis> {
+        try {
+            val analysesDtos = issues.map { issue ->
+                nlpApiService.analyzeIssue("${issue.title}: \n${issue.description}")    // TODO: parse issue labels
+            }
+            return analysesDtos.map { dto ->
+                dto.toDomain()
+            }
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun analyzeCodeComments(sourceCode: CodeSnippet): NlpAnalysis {
-        TODO("Not yet implemented")
+    override suspend fun analyzeCodeComments(sourceCode: CodeSnippet): NlpAnalysis {
+        try {
+            val analysisDto = nlpApiService.analyzeComment(sourceCode.body)
+            return analysisDto.toDomain()
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 
-    override fun analyzeFileComments(content: FileContent): NlpAnalysis {
-        TODO("Not yet implemented")
+    override suspend fun analyzeFileComments(content: FileContent): NlpAnalysis {
+        try {
+            val analysisDto = nlpApiService.analyzeComment(content.content)
+            return analysisDto.toDomain()
+        }
+        catch (e: Exception) {
+            throw DomainError()
+        }
     }
 }
