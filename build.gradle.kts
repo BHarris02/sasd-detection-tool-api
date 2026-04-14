46be887a11cce1f893f26f48daeaab717e36ec39
BHarris02 <dev.blake.harris@gmail.com>
2026-04-14 12:27:25 +0100

    add build configuration for Kotlin serialization and data module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
+	kotlin("plugin.serialization") version "2.2.21" apply false
 	id("org.springframework.boot") version "4.0.5"
 	id("io.spring.dependency-management") version "1.1.7"
 }
