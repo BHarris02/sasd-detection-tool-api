63011ffe871412d7586536a7e36ecb4c833711e5
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 12:26:31 +0100

    add `NlpGatewayImpl` shell

---
+++ data/src/main/kotlin/com/sasd/data/gateway/NlpGatewayImpl.kt
@@ -0,0 +1,26 @@
+package com.sasd.data.gateway
+
+import com.sasd.domain.entity.analysis.NlpAnalysis
+import com.sasd.domain.entity.vcs.CodeSnippet
+import com.sasd.domain.entity.vcs.Commit
+import com.sasd.domain.entity.vcs.FileContent
+import com.sasd.domain.entity.vcs.Issue
+import com.sasd.domain.gateway.NlpGateway
+
+class NlpGatewayImpl(): NlpGateway {
+    override fun analyzeCommits(commits: List<Commit>): List<NlpAnalysis> {
+        TODO("Not yet implemented")
+    }
+
+    override fun analyzeIssues(issues: List<Issue>): List<NlpAnalysis> {
+        TODO("Not yet implemented")
+    }
+
+    override fun analyzeCodeComments(sourceCode: CodeSnippet): NlpAnalysis {
+        TODO("Not yet implemented")
+    }
+
+    override fun analyzeFileComments(content: FileContent): NlpAnalysis {
+        TODO("Not yet implemented")
+    }
+}
