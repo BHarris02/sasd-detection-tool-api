6b86da24e403490d616a31cb1dfc12eb27df863f
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 14:26:54 +0100

    chore: update dependencies for Ktor client and serialization

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -11,7 +11,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation(project(":domain"))
+    // ktor
+    implementation("io.ktor:ktor-client-core:3.1.2")
+    implementation("io.ktor:ktor-client-cio:3.1.2")     // coroutines
+    implementation("io.ktor:ktor-client-content-negotiation:3.1.2")
+    implementation("io.ktor:ktor-client-core:3.1.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.2")
+    // testing
     testImplementation(kotlin("test"))
 }
 
