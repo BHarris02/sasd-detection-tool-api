7d402081a48d119642d297d4f7c833139b0d68d4
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-14 16:30:19 +0100

    fix: async methods

--- data/src/main/kotlin/com/sasd/data/client/analysis/NlpApiService.kt
+++ data/src/main/kotlin/com/sasd/data/client/analysis/NlpApiService.kt
@@ -3,7 +3,7 @@ package com.sasd.data.client.analysis
 import com.sasd.data.client.analysis.dto.NlpAnalysisDto
 
 interface NlpApiService {
-    fun analyzeCommit(commitMessage: String): NlpAnalysisDto
-    fun analyzeIssue(issue: String): NlpAnalysisDto
-    fun analyzeComment(comment: String): NlpAnalysisDto
+    suspend fun analyzeCommit(commitMessage: String): NlpAnalysisDto
+    suspend fun analyzeIssue(issue: String): NlpAnalysisDto
+    suspend fun analyzeComment(comment: String): NlpAnalysisDto
 }
