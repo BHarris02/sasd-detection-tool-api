c9334848a2c333df21af95c65ae77f65ed9a680e
BHarris02 <dev.blake.harris@gmail.com>
2026-04-14 11:03:19 +0100

    init app

---
+++ src/test/kotlin/com/sasd/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sasd.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
