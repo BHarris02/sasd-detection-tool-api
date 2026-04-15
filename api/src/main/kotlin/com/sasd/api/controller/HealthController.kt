e5b7f523b2c6540c445c87b5977440792b1fbec0
BHarris02 <dev.blake.harris@gmail.com>
2026-04-15 15:37:09 +0100

    add health controller

---
+++ api/src/main/kotlin/com/sasd/api/controller/HealthController.kt
@@ -0,0 +1,13 @@
+package com.sasd.api.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+    @GetMapping
+    fun health(): ResponseEntity<Void> = ResponseEntity.ok().build()
+}
