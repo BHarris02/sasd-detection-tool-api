9499fbdfdbc44d8f1001dc10ceed7a2fe0eeab18
BHarris02 <dev.blake.harris@gmail.com>
2026-04-15 12:34:04 +0100

    add config properties decorator

--- app/src/main/kotlin/com/sasd/app/BackendApplication.kt
+++ app/src/main/kotlin/com/sasd/app/BackendApplication.kt
@@ -1,9 +1,12 @@
 package com.sasd.app
 
+import com.sasd.app.config.AppConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppConfig::class)
 class BackendApplication
 
 fun main(args: Array<String>) {
