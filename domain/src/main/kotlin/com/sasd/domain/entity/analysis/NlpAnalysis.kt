129c3dea51f5d4459910c29e266e028b30efec09
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-15 09:16:47 +0100

    fix: possible nullable properties

--- domain/src/main/kotlin/com/sasd/domain/entity/analysis/NlpAnalysis.kt
+++ domain/src/main/kotlin/com/sasd/domain/entity/analysis/NlpAnalysis.kt
@@ -2,6 +2,6 @@ package com.sasd.domain.entity.analysis
 
 data class NlpAnalysis(
     val isSasd: Boolean,
-    val sasdAnalysis: SasdAnalysis,
-    val cweMapping: CweMapping
+    val sasdAnalysis: SasdAnalysis?,
+    val cweMapping: CweMapping?
 )
