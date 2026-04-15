84d1ed5350e8d67f738f89f7d7548311d96b2a97
BHarris02 <dev.blake.harris@gmail.com>
2026-04-15 15:33:45 +0100

    add component scan for API and application packages

--- app/src/main/kotlin/com/sasd/app/BackendApplication.kt
+++ app/src/main/kotlin/com/sasd/app/BackendApplication.kt
@@ -4,9 +4,11 @@ import com.sasd.app.config.AppConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig::class)
+@ComponentScan(basePackages = ["com.sasd.app", "com.sasd.api"])
 class BackendApplication
 
 fun main(args: Array<String>) {
