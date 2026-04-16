404d26a99bafb38b91d7ee87cc055394accabd3c
BHarris02 <dev.blake.harris@gmail.com>
2026-04-16 16:12:04 +0100

    add relevant artifacts to responses

--- api/src/main/kotlin/com/sasd/api/model/response/analysis/AnalyzeCodeCommentsResponse.kt
+++ api/src/main/kotlin/com/sasd/api/model/response/analysis/AnalyzeCodeCommentsResponse.kt
@@ -1,6 +1,7 @@
 package com.sasd.api.model.response.analysis
 
 data class AnalyzeCodeCommentsResponse(
+    val codeSnippet: String,
     val isSasd: Boolean,
     val explanation: String?,
     val severity: String?,
--- api/src/main/kotlin/com/sasd/api/model/response/analysis/AnalyzeFileCommentsResponse.kt
+++ api/src/main/kotlin/com/sasd/api/model/response/analysis/AnalyzeFileCommentsResponse.kt
@@ -1,6 +1,7 @@
 package com.sasd.api.model.response.analysis
 
 data class AnalyzeFileCommentsResponse(
+    val fileContent: String,
     val isSasd: Boolean,
     val explanation: String?,
     val severity: String?,
