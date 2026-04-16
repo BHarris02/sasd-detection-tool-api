29e07c6aa70c35d2a1ed238c2aff589afcaff074
BHarris02 <dev.blake.harris@gmail.com>
2026-04-16 14:29:54 +0100

    implement route handlers

--- api/src/main/kotlin/com/sasd/api/controller/VcsController.kt
+++ api/src/main/kotlin/com/sasd/api/controller/VcsController.kt
@@ -1,9 +1,11 @@
 package com.sasd.api.controller
 
+import com.sasd.api.mapper.toResponse
 import com.sasd.api.model.response.vcs.GetFileContentResponse
 import com.sasd.api.model.response.vcs.GetRepositoryStructureResponse
 import com.sasd.domain.common.DomainResult
 import com.sasd.domain.usecase.vcs.GetFileContentUseCase
+import com.sasd.domain.usecase.vcs.GetRepositoryStructureUseCase
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/vcs")
 class VcsController(
-    private val getFileContentUseCase: GetFileContentUseCase
+    private val getFileContentUseCase: GetFileContentUseCase,
+    private val getRepositoryStructureUseCase: GetRepositoryStructureUseCase
 ) {
     @GetMapping("/repository-structure")
-    fun getRepositoryStructure(@RequestParam repoUrl: String): GetRepositoryStructureResponse {
-        throw NotImplementedError()
+    suspend fun getRepositoryStructure(@RequestParam repoUrl: String): List<GetRepositoryStructureResponse> {
+        return when (val result = getRepositoryStructureUseCase(repoUrl)) {
+            is DomainResult.Success -> result.value.map { it.toResponse() }
+            is DomainResult.Failure -> throw result.error
+        }
     }
 
     @GetMapping("/file-content")
@@ -24,8 +30,7 @@ class VcsController(
         @RequestParam repoUrl: String,
         @RequestParam filePath: String
     ): GetFileContentResponse {
-        val result = getFileContentUseCase(repoUrl, filePath)
-        return when (result) {
+        return when (val result = getFileContentUseCase(repoUrl, filePath)) {
             is DomainResult.Success -> GetFileContentResponse(result.value.content)
             is DomainResult.Failure -> throw result.error
         }
