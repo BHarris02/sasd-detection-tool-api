829b889ad42af217d2552be639782175e86764da
BHarris02 <dev.blake.harris@gmail.com>
2026-04-17 15:11:15 +0100

    implement ClaudeApiService.kt

--- app/src/main/kotlin/com/sasd/app/di/DataConfiguration.kt
+++ app/src/main/kotlin/com/sasd/app/di/DataConfiguration.kt
@@ -2,6 +2,7 @@ package com.sasd.app.di
 
 import com.sasd.app.config.AppConfig
 import com.sasd.data.client.analysis.NlpApiService
+import com.sasd.data.client.analysis.impl.anthropic.ClaudeApiService
 import com.sasd.data.client.analysis.impl.google.GeminiApiService
 import com.sasd.data.client.vcs.VcsApiService
 import com.sasd.data.client.vcs.impl.github.GitHubApiService
@@ -26,10 +27,11 @@ class DataConfiguration(private val config: AppConfig) {
     fun vcsRepository(vcsApiService: VcsApiService): VcsRepository = VcsRepositoryImpl(vcsApiService)
 
     @Bean
-    fun nlpApiService(): NlpApiService = GeminiApiService(
-        config.googleToken,
-        config.googleModel
-    )
+    fun nlpApiService(): NlpApiService = when (config.llmProvider) {
+        "google" -> GeminiApiService(config.llmApiKey, config.llmModel)
+        "anthropic" -> ClaudeApiService(config.llmApiKey, config.llmModel)
+        else -> throw IllegalArgumentException("Unsupported LLM provider: ${config.llmProvider}")
+    }
 
     @Bean
     fun nlpGateway(nlpApiService: NlpApiService): NlpGateway = NlpGatewayImpl(nlpApiService)
